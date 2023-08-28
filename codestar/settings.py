d77b03c71f468296b149982e8aa96afa964d03f5
Diarmuid Sheeran <dssheeran@outlook.com>
2023-08-28 08:55:34 +0000

    Update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-diarmuidshee-djangoblog-jreotq8kgnm.ws-eu104.gitpod.io', 'https://django-ci-blog-c1aec6de1ed9.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-diarmuidshee-djangoblog-jreotq8kgnm.ws-eu104.gitpod.io', 'django-ci-blog-c1aec6de1ed9.herokuapp.com', 'localhost']
 
 
 
